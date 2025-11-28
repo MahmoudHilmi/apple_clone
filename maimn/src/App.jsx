49f8b5b12b5f009ac79d57c7fe4a6b60317b5d76
Mahmoud_Hilmi <m.hilmiharb1@gmail.com>
2025-11-28 13:37:39 +0200

    temporary Deploy

--- maimn/src/App.jsx
+++ maimn/src/App.jsx
@@ -1,15 +1,14 @@
 import "./index.css";
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
+import ProductViewer from "./components/productViewer";
 
 function App() {
   return (
     <main className="">
       <Navbar />
       <Hero />
-
-
-
+      <ProductViewer />
     </main>
   );
 }
