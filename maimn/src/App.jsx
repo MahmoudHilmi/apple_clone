11e01bc5fa5841ca9503ed53c70cb57eaa176fae
Mahmoud_Hilmi <m.hilmiharb1@gmail.com>
2025-12-01 14:42:35 +0200

    Temporary Deploy

--- maimn/src/App.jsx
+++ maimn/src/App.jsx
@@ -2,10 +2,15 @@ import "./index.css";
 import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import ProductViewer from "./components/productViewer";
+import Showcase from "./components/Showcase";
+import Performence from "./components/Performence";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
@@ -13,6 +18,11 @@ function App() {
       <Navbar />
       <Hero />
       <ProductViewer />
+      <Showcase />
+      <Performence />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   );
 }
