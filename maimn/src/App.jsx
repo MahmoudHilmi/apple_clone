2373d54b038beac6fe866be73e02d0cdf3cf2b2d
Mahmoud_Hilmi <m.hilmiharb1@gmail.com>
2025-12-01 12:50:39 +0200

    Showcase Section

--- maimn/src/App.jsx
+++ maimn/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import ProductViewer from "./components/productViewer";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Showcase from "./components/Showcase";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 }
