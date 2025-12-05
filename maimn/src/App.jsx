d367f78092db3330f739a0e9110822250bb8ea18
Mahmoud_Hilmi <m.hilmiharb1@gmail.com>
2025-12-05 19:40:08 +0200

    Performance Initialize

--- maimn/src/App.jsx
+++ maimn/src/App.jsx
@@ -4,6 +4,12 @@ import Hero from "./components/Hero";
 import ProductViewer from "./components/productViewer";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Performance from "./components/Performance";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer"
+
+
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,6 +19,10 @@ function App() {
       <Navbar />
       <Hero />
       <ProductViewer />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   );
 }
